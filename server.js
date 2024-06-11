26640bdbb770dbfd3a5946b98c9e0aa442942386
Gabriel Schultz <gabrielschultz2807@gmail.com>
2024-06-10 22:46:37 -0300

    Adicionado logica de login

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ import bodyParser from "body-parser";
 import express from "express";
 import mysql from "mysql2";
 import { error } from "console";
+import jwt from "jsonwebtoken";
 
+const SEGREDO = 'REMOTA';
 const app = express();
 const porta = 3000;
 const conexao = mysql.createConnection({
@@ -29,6 +31,38 @@ app.get("/", (req, resp)=> {
 app.get("/servicos", (req, res) => {
     res.status(200).send("rotas para trazer o servicos");
 });
+
+let html = '';
+
+// middleware
+function verificarToken(req, res, next){
+    const token = req.headers['x-access-token'];
+    jwt.verify(token, SEGREDO, (erro, decodificado) => {
+      if(erro)
+        return res.status(401).end();
+      req.id = decodificado.id;
+      next();
+    });
+  }
+  
+  app.post("/login", (req, res) => {
+    let usu = req.body.usuario;
+    let sen = req.body.senha;
+  
+    // conectar ao bd pra buscar o ID desse usuario
+  
+    //if usu e a senha for igual ao registrado na tabela do BD
+    if(usu == "marcos" && sen == "123"){
+      const id = 1; // isso vem do BD
+  
+      //token tem 3 partes = 1.) identifica o usuário 2.) segredo, opções 
+      const token = jwt.sign({id}, SEGREDO, { expiresIn: 300}); // 5 min
+  
+      console.log("usuário marcos logou no sistema");
+      return res.status(500).json({autenticado: true, token: token});
+    };
+    res.status(504).send("Usuário inválido ou inexitente");
+  });
 
 app.post("/servicos", (req, res) => {
     let tit = req.body.titulo;
