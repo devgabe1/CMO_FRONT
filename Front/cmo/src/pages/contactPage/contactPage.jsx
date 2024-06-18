9b9d9e5dc517d9b0347bbd2dff4bc67de45cb025
Gabe <gabrielschultz2807@gmail.com>
2024-06-18 17:54:44 -0300

    Centralizado texto do formulário

--- Front/cmo/src/pages/contactPage/contactPage.jsx
+++ Front/cmo/src/pages/contactPage/contactPage.jsx
@@ -1,5 +1,6 @@
 // src/pages/LocationPage.jsx
 import React from 'react';
+import './contactPage.css';
 import ChoiceBar from '../../components/choiceBar/choiceBar';
 import Form from '../../components/form/form';
 
@@ -9,8 +10,11 @@ function contactPage() {
       <div className="main-content"> 
       <ChoiceBar />
       </div>
+      <div className='page-container'>
       <h1>Contato</h1>
       <p>Bem-vindo à página de contato.</p>
+      <p>Preencha o formulário abaixo e entraremos em contato.</p>
+      </div>
       <Form />
     </div>
   );
