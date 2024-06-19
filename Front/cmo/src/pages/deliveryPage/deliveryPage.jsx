6c46adf2c8eace5ca2a0ed1c19e859f4aacd3b76
Gabriel Schultz <gabrielschultz2807@gmail.com>
2024-06-18 23:21:36 -0300

    Adicionado conteudo a pagina de delivery

--- Front/cmo/src/pages/deliveryPage/deliveryPage.jsx
+++ Front/cmo/src/pages/deliveryPage/deliveryPage.jsx
@@ -1,17 +1,28 @@
 // src/pages/LocationPage.jsx
 import React from 'react';
 import ChoiceBar from '../../components/choiceBar/choiceBar';
+import './deliveryPage.css'; // Importa o arquivo de estilos CSS
 
-function deliveryPage() {
+function DeliveryPage() {
+  const handleWhatsAppClick = () => {
+    const phoneNumber = '5511999999999'; // Substitua pelo número de telefone da sua loja com o código do país
+    const message = 'Olá, gostaria de fazer um pedido de delivery.';
+    const url = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+    window.open(url, '_blank');
+  };
+
   return (
-    <div>
+    <div className="page-container">
       <div className="main-content"> 
-      <ChoiceBar />
+        <ChoiceBar />
       </div>
-      <h1>Busca e Entrega</h1>
-      <p>Bem-vindo à página de Busca e entrega.</p>
+      <h1>Busca e entrega em Curitiba</h1>
+      <p>Realize seu pedido pelo WhatsApp e agende um horário.</p>
+      <button className="whatsapp-button" onClick={handleWhatsAppClick}>
+        Pedir pelo WhatsApp
+      </button>
     </div>
   );
 }
 
-export default deliveryPage;
+export default DeliveryPage;
