9490b2997fcda8c710de7fac37744f1960b62f53
Gabe <gabrielschultz2807@gmail.com>
2024-06-28 16:08:30 -0300

    HomePage fix

--- src/pages/homePage/homePage.jsx
+++ src/pages/homePage/homePage.jsx
@@ -4,18 +4,42 @@ import '../../styles/global.css';
 import Carousel from '../../components/carousel/carousel';
 import ChoiceBar from '../../components/choiceBar/choiceBar';
 import './homePage.css';
+import { useNavigate } from 'react-router-dom';
+
+const handleWhatsAppClick = () => {
+  const phoneNumber = '554137950162';
+  const message = 'Olá, gostaria de fazer um pedido de busca/entrega.';
+  const url = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+  window.open(url, '_blank'); 
+};
 
 const HomePage = () => {
+  const navigate = useNavigate();
   return (
     <div className='page-backgroundHome'>
       <div className="home-page-container">
         <div className="main-content"> 
           <ChoiceBar />
         </div>
-        <Carousel /> {/* Adiciona o Carousel aqui */}
         <div className="home-content">
-          <h1>Bem-vindo à Página Inicial</h1>
-          <p>Conteúdo da sua página inicial aqui...</p>
+          <h1>Assistência Técnica para as Melhores Marcas!</h1>
+          <Carousel />
+          <div className="content-section">
+            <div className="content-left">
+              <p className='p3'>Peça já seu orçamento!</p>
+              <p className='p3'>Contate-nos, buscamos e entregamos seu aparelho</p>
+              <button className="whatsapp-button" onClick={handleWhatsAppClick}>
+                Pedir pelo WhatsApp
+              </button>
+            </div>
+            <div className="content-right">
+              <p className='p3'>Compareça a uma das lojas físicas!</p>
+              <p className='p3'>Ambas as lojas contam com estacionamento!</p>
+              <button className="whatsapp-button" onClick={() => navigate('/localizacao')}>
+                Endereços
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
